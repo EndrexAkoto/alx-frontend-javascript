79e9fefb3880c2931f78d55a76e20bd32a3d33a0
EndrexAkoto <martinakoto3@gmail.com>
2024-01-04 10:29:23 +0000

    2-main.js

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,7 +1,6 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  // Use arrow function for addNeighborhood
   this.addNeighborhood = (newNeighborhood) => {
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
     return this.sanFranciscoNeighborhoods;
