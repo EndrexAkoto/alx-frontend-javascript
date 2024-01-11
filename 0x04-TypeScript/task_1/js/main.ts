30d94a9eaa27b557f87ea0507e83467593b22f98
EndrexAkoto <martinakoto3@gmail.com>
2024-01-11 17:58:54 +0000

    changes

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,6 +1,4 @@
-
 // Task 1 - Teacher Interface
-
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
@@ -22,7 +20,6 @@ console.log("Task 1 - Teacher Interface:");
 console.log(teacher3);
 
 // Task 2 - Extending the Teacher Class
-
 interface Directors extends Teacher {
   numberOfReports: number;
 }
@@ -37,4 +34,43 @@ const director1: Directors = {
 
 console.log("\nTask 2 - Extending the Teacher Class:");
 console.log(director1);
+
+// Additional Task - printTeacherFunction
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  const firstLetter = firstName.charAt(0).toUpperCase();
+  const fullName = `${firstLetter}. ${lastName}`;
+  return fullName;
+};
+
+// Task 4 - Writing a class
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClass;
+}
+
+interface StudentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+const StudentClass: StudentConstructor = class StudentClass implements StudentClass {
+  constructor(private firstName: string, private lastName: string) {}
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+};
+
+const student1: StudentClass = new StudentClass('Alice', 'Johnson');
+
+console.log("\nTask 4 - Writing a class:");
+console.log(student1.displayName());
+console.log(student1.workOnHomework());
 
